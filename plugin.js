041450a4f2d77156501b98f24797712cf94ae8c1
guruahn <guruahn@gmail.com>
2015-06-15 17:31:42 +0900

    add dialog for markdown editing when we use INLINE MODE
    add dialog for markdown editing when we use INLINE MODE

--- plugin.js
+++ plugin.js
@@ -13,9 +13,6 @@
         icons: 'markdown',
         hidpi: true, // %REMOVE_LINE_CORE%
         init: function(editor) {
-            // Source mode in inline editors is only available through the "sourcedialog" plugin.
-            if (editor.elementMode == CKEDITOR.ELEMENT_MODE_INLINE)
-                return;
 
             var markdown = CKEDITOR.plugins.markdown,
                 rootPath = this.path,
@@ -26,72 +23,98 @@
                 };
             editor.config.markdown = CKEDITOR.tools.extend(defaultConfig, editor.config.markdown || {}, true);
 
-            editor.addMode('markdown', function(callback) {
-                var contentsSpace = editor.ui.space('contents'),
-                    textarea = contentsSpace.getDocument().createElement('textarea'),
-                    config = editor.config.markdown;
+            // Source mode in inline editors is only available through the "sourcedialog" plugin.
+            if (editor.elementMode == CKEDITOR.ELEMENT_MODE_INLINE){
+                editor.addCommand( 'markdowndialog', new CKEDITOR.dialogCommand( 'markdowndialog' ) );
+                // Register the "source" dialog.
+                CKEDITOR.dialog.add( 'markdowndialog', this.path + '/js/markdowndialog.js' );
 
-                textarea.setStyles(
-                    CKEDITOR.tools.extend({
-                            // IE7 has overflow the <textarea> from wrapping table cell.
-                            width: CKEDITOR.env.ie7Compat ? '99%' : '100%',
-                            height: '100%',
-                            resize: 'none',
-                            outline: 'none',
-                            'text-align': 'left'
-                        },
-                        CKEDITOR.tools.cssVendorPrefix('tab-size', editor.config.sourceAreaTabSize || 4)));
+                editor.ui.addButton('markdowndialog', {
+                    label: 'markdowndialog',
+                    command: 'markdowndialog',
+                    toolbar: 'insert,0',
+                    icon: this.path + 'icons/markdown.png'
+                });
+            }else{
 
-                // Make sure that source code is always displayed LTR,
-                // regardless of editor language (#10105).
-                textarea.setAttribute('dir', 'ltr');
+                editor.addMode('markdown', function(callback) {
+                    var contentsSpace = editor.ui.space('contents'),
+                        textarea = contentsSpace.getDocument().createElement('textarea'),
+                        config = editor.config.markdown;
 
-                textarea.addClass('cke_source cke_reset cke_enable_context_menu');
+                    textarea.setStyles(
+                        CKEDITOR.tools.extend({
+                                // IE7 has overflow the <textarea> from wrapping table cell.
+                                width: CKEDITOR.env.ie7Compat ? '99%' : '100%',
+                                height: '100%',
+                                resize: 'none',
+                                outline: 'none',
+                                'text-align': 'left'
+                            },
+                            CKEDITOR.tools.cssVendorPrefix('tab-size', editor.config.sourceAreaTabSize || 4)));
 
-                editor.ui.space('contents').append(textarea);
+                    // Make sure that source code is always displayed LTR,
+                    // regardless of editor language (#10105).
+                    textarea.setAttribute('dir', 'ltr');
 
-                var editable = editor.editable(new sourceEditable(editor, textarea)),
-                    htmlData = editor.getData(1);
+                    textarea.addClass('cke_source cke_reset cke_enable_context_menu');
 
-                // Convert to Markdown and Fill the textarea.
-                if (typeof(toMarkdown) == 'undefined') {
-                    CKEDITOR.scriptLoader.load(rootPath + 'js/to-markdown.js', function() {
+                    editor.ui.space('contents').append(textarea);
+
+                    var editable = editor.editable(new sourceEditable(editor, textarea)),
+                        htmlData = editor.getData(1);
+
+                    // Convert to Markdown and Fill the textarea.
+                    if (typeof(toMarkdown) == 'undefined') {
+                        CKEDITOR.scriptLoader.load(rootPath + 'js/to-markdown.js', function() {
+                            editable.setData(toMarkdown(htmlData));
+                        });
+                    } else {
                         editable.setData(toMarkdown(htmlData));
-                    });
-                } else {
-                    editable.setData(toMarkdown(htmlData));
-                }
-
-                if (typeof (CodeMirror) == 'undefined' || typeof (CodeMirror.modes.gfm) == 'undefined') {
-                    CKEDITOR.document.appendStyleSheet(rootPath + 'css/codemirror.min.css');
-                
-                    if (config.theme.length && config.theme != 'default') {
-                        CKEDITOR.document.appendStyleSheet(rootPath + 'theme/' + config.theme + '.css');
                     }
 
-                    CKEDITOR.scriptLoader.load(rootPath + 'js/codemirror-gfm-min.js', function() {
-                        loadCodeMirror(editor, editable);
-                    });
-                } else {
-                    loadCodeMirror(editor, editable);
-                }
-                if (typeof(marked) == 'undefined') {
-                    CKEDITOR.scriptLoader.load(rootPath + 'js/marked.js');
-                }
-                // Having to make <textarea> fixed sized to conquer the following bugs:
-                // 1. The textarea height/width='100%' doesn't constraint to the 'td' in IE6/7.
-                // 2. Unexpected vertical-scrolling behavior happens whenever focus is moving out of editor
-                // if text content within it has overflowed. (#4762)
-                if (CKEDITOR.env.ie) {
-                    editable.attachListener(editor, 'resize', onResize, editable);
-                    editable.attachListener(CKEDITOR.document.getWindow(), 'resize', onResize, editable);
-                    CKEDITOR.tools.setTimeout(onResize, 0, editable);
-                }
+                    if (typeof (CodeMirror) == 'undefined' || typeof (CodeMirror.modes.gfm) == 'undefined') {
+                        CKEDITOR.document.appendStyleSheet(rootPath + 'css/codemirror.min.css');
 
-                editor.fire('ariaWidget', this);
-                editor.commands.maximize.modes.markdown = 1;
-                callback();
-            });
+                        if (config.theme.length && config.theme != 'default') {
+                            CKEDITOR.document.appendStyleSheet(rootPath + 'theme/' + config.theme + '.css');
+                        }
+
+                        CKEDITOR.scriptLoader.load(rootPath + 'js/codemirror-gfm-min.js', function() {
+                            loadCodeMirror(editor, editable);
+                        });
+                    } else {
+                        loadCodeMirror(editor, editable);
+                    }
+                    if (typeof(marked) == 'undefined') {
+                        CKEDITOR.scriptLoader.load(rootPath + 'js/marked.js');
+                    }
+                    // Having to make <textarea> fixed sized to conquer the following bugs:
+                    // 1. The textarea height/width='100%' doesn't constraint to the 'td' in IE6/7.
+                    // 2. Unexpected vertical-scrolling behavior happens whenever focus is moving out of editor
+                    // if text content within it has overflowed. (#4762)
+                    if (CKEDITOR.env.ie) {
+                        editable.attachListener(editor, 'resize', onResize, editable);
+                        editable.attachListener(CKEDITOR.document.getWindow(), 'resize', onResize, editable);
+                        CKEDITOR.tools.setTimeout(onResize, 0, editable);
+                    }
+
+                    editor.fire('ariaWidget', this);
+                    editor.commands.maximize.modes.markdown = 1;
+                    callback();
+                });
+
+                editor.addCommand('markdown', markdown.commands.markdown);
+
+                if (editor.ui.addButton) {
+                    editor.ui.addButton('Markdown', {
+                        label: 'Markdown',
+                        command: 'markdown'
+                        // toolbar: 'mode,10'
+                    });
+                }
+            }
+
 
             function loadCodeMirror(editor, editable){
                 window["codemirror_" + editor.id] = CodeMirror.fromTextArea(editable.$, editor.config.markdown);
@@ -100,13 +123,6 @@
 
             editor.addCommand('markdown', markdown.commands.markdown);
 
-            if (editor.ui.addButton) {
-                editor.ui.addButton('Markdown', {
-                    label: 'Markdown',
-                    command: 'markdown'
-                    // toolbar: 'mode,10'
-                });
-            }
             CKEDITOR.document.appendStyleText('.cke_button__markdown_label {display: inline;}'); // display button Label
             editor.on('mode', function() {
                 editor.getCommand('markdown').setState(editor.mode == 'markdown' ? CKEDITOR.TRISTATE_ON : CKEDITOR.TRISTATE_OFF);
